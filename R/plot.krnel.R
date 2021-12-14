fe849eda6540ca77244376b02d19c2cb76d274c1
jframi <jframi@gmail.com>
2021-12-14 20:03:42 +0100

    Added parameter for features custom colors in plot.krnel

--- R/plot.krnel.R
+++ R/plot.krnel.R
@@ -11,15 +11,19 @@
 #' @export
 #'
 #' @examples
-plot.krnel<-function(x, label=TRUE, label.cex=0.6, label.col="red", plot.bbox=T, bbox.col="blue", title=T, plot.radii=T, plot.ellipse=T, plot.ws.bbox=F, ws.bbox.col="pink", plot.pois=F, plot.ws.pois=F, poi.col="lightblue", poi.ws.col="lightblue"){
+plot.krnel<-function(x, label=TRUE, label.cex=0.6, label.col="red", plot.bbox=T, bbox.col="blue", title=T, plot.radii=T, plot.ellipse=T, plot.ws.bbox=F, ws.bbox.col="pink", plot.pois=F, plot.ws.pois=F, poi.col="lightblue", poi.ws.col="lightblue", custom.col=NULL){
   if (title){
     plot(0,0, type="n",xlim=c(0,x$params$w),ylim=c(0,x$params$h), xlab="", ylab="", asp = 1, main=x$params$image.name)
   }else{
     plot(0,0, type="n",xlim=c(0,x$params$w),ylim=c(0,x$params$h), xlab="", ylab="", asp = 1)
   }
-
+  if (!is.null(custom.col) & length(custom.col)!=nrow(x$features)) stop("custom.col should have the same length as the number of features")
   for (g in 1:length(x$contours)){
-    polygon(x$contours[[g]][,1],x$params$h-x$contours[[g]][,2],col = x$features$cols[g], border = NA)
+    if (is.null(custom.col)){
+      polygon(x$contours[[g]][,1],x$params$h-x$contours[[g]][,2],col = x$features$cols[g], border = NA)
+    }else{
+      polygon(x$contours[[g]][,1],x$params$h-x$contours[[g]][,2],col = custom.col[g], border = NA)
+    }
   }
   if (label){
     text(x = x$features$m.cx, y = x$params$h-x$features$m.cy, label=x$features$id, cex=label.cex, col = label.col)
